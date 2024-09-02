f38149d06adc13f9e7299554300233c42b466484
Alessio1599 <alessio.guarachi@studio.unibo.it>
2024-09-02 16:26:21 +0200

    update main

--- OpenAI_API/freecodecamp.py
+++ OpenAI_API/freecodecamp.py
@@ -19,10 +19,17 @@ personal_trainer_assistant = client.beta.assistants.create(
 assistant_id = personal_trainer_assistant.id
 print(assistant_id)
 
+
+
 #== Thread ==
 # series of interactions or messages between you and the model that are related to a single topic or conversation.
+
+# this code snippet creates a new conversation thread with an initial message from the user 
+# asking about how to start working out to lose fat and build muscle.
+
+# client.beta.threads.create(...): This function call creates a new thread or conversation.
 thread = client.beta.threads.create(
-    messages=[
+    messages=[ # messages=[{...}]: This specifies the content of the initial message(s) in the thread. 
         {
             "role": "user",
             "content": "How do I get started working out to loose fat and build muscles"
@@ -32,3 +39,15 @@ thread = client.beta.threads.create(
 )
 thread_id = thread.id
 print(thread_id)
+
+# Running the previous part of code I've created an assistant called "Personal Trainer" and a thread with a message from the user asking how to get started working out to loose fat and build muscles. Now I will use the assistant to respond to the user's message.
+
+# == Hardcode our ids ==
+assistant_id = 'asst_hhUxKDVQaYfUBnBh5V4lclka'
+thread_id = 'thread_A83ryKXAIDKTa71iA8rrl0SE'
+
+# == Create a Message ==
+message = "What are the best exercises for lean muscles and getting rid of fat?"
+message = client.beta.threads.messages.create(
+    
+)
